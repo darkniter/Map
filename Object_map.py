30d0460dc075ff35b11ab7039d7c9f2768e25221
darkniter <Poranot@yandex.ru>
2019-06-10 17:20:25 +0300

    parse into json and back

--- Object_map.py
+++ Object_map.py
@@ -1,31 +1,59 @@
 import configobj
-options = ('Address','Name','ID','Group')
-itog = {}
-count = -1
-conf = configobj.ConfigObj('smartintel.1.map')
+import json
+import os
 
-# print(conf)
+def map_parse (options, fname):
+    conf = configobj.ConfigObj(fname)
+    result = {}
 
-for elements in conf:
-    slov={}
 
-    el = conf[elements]
+    for elements in conf:
+        slov={}
 
-    count += 1
+        el = conf[elements]
 
-    for values in el:
-        if (values == 'Hint'):
-            if isinstance(el[values],str):
-                param = el[values]
-            else:
-                for init in el[values]:
-                    param = '(' + init + '|' + str(el[values][init]) + ')'
 
-            slov.update({values:param})
-        if (values == 'Addresses'):
-            adresses = el[values].split(';')
-            slov.update({values:adresses})
-        if(values in options):
-            slov.update({values:el[values]})
-    itog.update({elements:slov})
-print (itog['Device222'])
+        for values in el:
+            if (values == 'Hint'):
+                if isinstance(el[values],str):
+                    param = el[values]
+                else:
+                    for init in el[values]:
+                        param = '(' + init + '|' + str(el[values][init]) + ')'
+
+                slov.update({values:param})
+            if (values == 'Addresses'):
+                adresses = el[values].split(';')
+                slov.update({values:adresses})
+            if(values in options):
+                slov.update({values:el[values]})
+        result.update({elements:slov})
+
+    return result
+
+
+def into_json(result):
+
+    if (os.path.isfile('map.json')):
+        os.remove('map.json')
+
+    json_file = open('map.json','a+')
+    json_file.write( json.dumps (result))
+    json_file.close()
+
+    return json_file.name
+
+def into_python (map):
+    json_map = open(map,'r')
+    python_map = json.load(json_map)
+    return python_map
+
+def main():
+    options = ('Address','Name','ID','Group')
+    result = map_parse(options,'smartintel.1.map')
+    name_json_file = into_json(result)
+    python_object_map = into_python(name_json_file)
+    print (type(python_object_map))
+
+if __name__ == "__main__":
+    main()
